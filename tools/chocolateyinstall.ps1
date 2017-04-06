f41a3385d2bd50cbe134eaa420d9151415752a91
dgrittma <david.grittmann@intlfcstone.com>
2017-04-06 11:01:29 -0500

    Updated Install.ps1 - Removed x64 references

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,11 +3,8 @@ $packageArgs = @{
   packageName    = $packageName
   fileType       = 'msi'
   url            = 'https://files.dynatrace.com/downloads/OnPrem/dynaTrace/6.5/6.5.0.1289/dynatrace-client-6.5.0.1289-x86-64.msi'
-  url64          = 'https://files.dynatrace.com/downloads/OnPrem/dynaTrace/6.5/6.5.0.1289/dynatrace-client-6.5.0.1289-x86-64.msi'
   checksum       = '9E722947C0EA90BE29F69D941F45C892EFCBC5CF77BFEA2F55E0E3584C93A579'
   checksumType   = 'sha256'
-  checksum64     = '9E722947C0EA90BE29F69D941F45C892EFCBC5CF77BFEA2F55E0E3584C93A579'
-  checksumType64 = 'sha256'
   silentArgs     = '/quiet'
   softwareName   = 'Dynatrace Client 6.5 (x64)'
 }
