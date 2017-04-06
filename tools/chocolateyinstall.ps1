9ad6590b70aa54fec31ddedc84fa588da81d3569
dgrittma <david.grittmann@intlfcstone.com>
2017-04-06 11:06:40 -0500

    Updates Install.ps1 - Software Name field adjusted

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,6 +6,6 @@ $packageArgs = @{
   checksum       = '9E722947C0EA90BE29F69D941F45C892EFCBC5CF77BFEA2F55E0E3584C93A579'
   checksumType   = 'sha256'
   silentArgs     = '/quiet'
-  softwareName   = 'Dynatrace Client 6.5 (x64)'
+  softwareName   = 'Dynatrace Client 6.5'
 }
   Install-ChocolateyPackage @packageArgs
